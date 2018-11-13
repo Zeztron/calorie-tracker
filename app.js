b3f8f2fe34dd1f5f55d63086334b5da3839f769a
Zeztron <pateltharsh@gmail.com>
2018-11-13 17:42:18 -0500

    added controller methods to add to item list

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ const ItemCtrl = (function() { // <--- this is calked in iffy
 
     // Public methods
     return {
+        getItems: function() {
+            return data.items;
+        },
         logData: function() {
             return data;
         }
@@ -32,12 +35,28 @@ const ItemCtrl = (function() { // <--- this is calked in iffy
 
 // UI controller
 const UICtrl = (function() { // <--- this is called in iffy
+    const UISelectors = {
+        itemList: "#item-list"
+    }
 
     // Public methods
     return {
-
+        populateItemList: function(items) {
+            let html = "";
+            items.forEach(function(item) {
+                html += `
+                <li class="collection-item" id="item-${item.id}">
+                    <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                    <a href="#" class="secondary-content">
+                        <i class="edit-item fas fa-pencil-alt"></i>
+                    </a>
+                </li>`;
+            });
+            // Insert list items
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+        }
     }
-});
+})();
 
 
 
@@ -47,7 +66,11 @@ const App = (function(ItemCtrl, UICtrl) {
     // Public methods
     return {
         init: function() {
-            console.log("Initializing App...");
+            // Fetch items from data structure
+            const items = ItemCtrl.getItems();
+            // Populate list with items
+            UICtrl.populateItemList(items);
+
         }
     }
 
